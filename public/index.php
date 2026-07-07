b017d605ceddf963ac3e440b8c631e366a8b7e74
DeemMoor <d.muravyev@webpractik.ru>
2026-07-07 16:08:39 +0300

    revert(public): убрать демо-сайт сайтокорпората из public/
    public/ возвращён к чистому docroot: index.php + симлинки bitrix/local/upload.
    Ошибочно закоммиченная в f2c59d4 демка (company/contacts/services/news/
    products/search/login/include, меню, .htaccess/urlrewrite.php со старыми
    короткими тегами) не имеет отношения к болванке и удалена.

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
+
+/** @var \CMain $APPLICATION */
+$APPLICATION->SetTitle('Bitrix boilerplate');
+?>
+Bitrix boilerplate
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php';
